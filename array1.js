401d16e697712922343703f6a2a890982759518f
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-26 21:51:06 +0600

    array slice and sort

--- array1.js
+++ array1.js
@@ -2,10 +2,18 @@
 
 var numberArray = [30, 32,34,80,90,59];
 var result= numberArray.reverse();
-console.log(result);
+// console.log(result);
 
 numberArray.push(12);
-console.log(numberArray);
+// console.log(numberArray);
 
 numberArray.pop();
-console.log(numberArray);
+// console.log(numberArray);
+
+// slice and sort method
+
+var numbersArray = [80,90, 30, 32,34,59];
+// console.log(numbersArray.sort().reverse());
+var result1= numbersArray.slice(1,3);
+console.log(result1);
+
