ebe20c50e4e9739bc1d4a5030297761b51bb8bf8
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-26 20:18:47 +0600

    array from method

--- array.js
+++ array.js
@@ -1,5 +1,13 @@
+// array concat
 var arr1 = ['A', 'B', 'C'];
 var arr2 = ['D', 'E', 'F'];
 
 var arr3 = arr1.concat(arr2);
 console.log(arr3);
+
+// array.from()=> string ke array te convert kore.
+
+var name = 'Ahasan Habib Nahid'
+
+var nameArray = Array.from(name);
+console.log(nameArray);
