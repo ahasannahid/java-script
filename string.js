530f97e73ba965c3052caf36a78736b9faf105fc
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-26 22:16:02 +0600

    substr and replace method

--- string.js
+++ string.js
@@ -1,11 +1,16 @@
 // concat and charAt method
 var country = "Bangladesh";
-// console.log(country.charAt(0));
 var country1 = "India";
+// console.log(country.charAt(0));
 // console.log(country.concat(country1));
 
 // indexOf and last indexOf (char exist na korle index -1)
 // console.log(country.indexOf('a'))
 // console.log(country.lastIndexOf('l'))
+
+// replace and substr method
+// console.log(country.replace('desh','deshi'));
+console.log(country.substr(0,4))
+
 
 
