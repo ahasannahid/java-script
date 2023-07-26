ffb83c80711f7d81196b138ea0702c58d620bad4
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-26 21:27:48 +0600

    array push and pop

--- array1.js
+++ array1.js
@@ -3,3 +3,9 @@
 var numberArray = [30, 32,34,80,90,59];
 var result= numberArray.reverse();
 console.log(result);
+
+numberArray.push(12);
+console.log(numberArray);
+
+numberArray.pop();
+console.log(numberArray);
