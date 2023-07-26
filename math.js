cb6e4ee4bd9c1d0da25ce032ad4a55f6efe7bd4b
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-26 23:33:17 +0600

    math object

---
+++ math.js
@@ -0,0 +1,11 @@
+// static obosthay thakle object create korte hoyna. direct class dorei method ke call kora jay.
+
+var number = -7;
+var number1 = 7.5;
+// console.log(Math.abs(number));
+// console.log(Math.ceil(number1));
+// console.log(Math.floor(number1));
+// console.log(Math.max(22,23,37,21,67));
+// console.log(Math.max(22,23,37,21,67));
+// console.log(Math.random());//0-1 er mjy random number return kore math.random()
+// console.log(Math.round(number1));
