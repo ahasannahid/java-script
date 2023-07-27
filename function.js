1e4e46edb293cf969fd0bd74ccaf1abc3a54552a
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-27 22:43:19 +0600

    async function

--- function.js
+++ function.js
@@ -4,10 +4,29 @@
 // Regular function
 function onLogIn(){
     console.log("LogIn success. Regular function!");
-}
+};
 onLogIn();
 // Arrow function
 const onLogIn1 = () => {
     console.log("LogIn success. Arrow function!");
-}
+};
 onLogIn1();
+
+// async function.
+
+/*
+ * Why we use async?
+ei function execute er age onno kisu execute hobe na . eijonne async use kora hoy.
+
+ */ 
+
+async function onSignUp (){
+    console.log('I am sign up')
+};
+
+const onLogIn2 = async() => {
+    console.log("LogIn success. Async Arrow function!");
+};
+
+onLogIn2();
+onSignUp();
