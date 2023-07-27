580147246ab1504e14b5372a7b5efc7fcf77f96b
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-27 22:38:54 +0600

    arrow function

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
 // normally function er name verbe e likha better.
 // function er different keyword, structure ke function signature bola hoy
+
+// Regular function
 function onLogIn(){
-    console.log("Log in success")
+    console.log("LogIn success. Regular function!");
 }
 onLogIn();
+// Arrow function
+const onLogIn1 = () => {
+    console.log("LogIn success. Arrow function!");
+}
+onLogIn1();
