301f5935233113a5ac8e98d67936769cb3c63efb
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-27 22:34:35 +0600

    regular function

---
+++ function.js
@@ -0,0 +1,6 @@
+// normally function er name verbe e likha better.
+// function er different keyword, structure ke function signature bola hoy
+function onLogIn(){
+    console.log("Log in success")
+}
+onLogIn();
